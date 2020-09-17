230d9b96b416a0041e2204f4c48006f3d48f0573
Sanad <s_wj_88@hotmail.com>
2020-09-18 00:44:06 +0300

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_212835) do
+ActiveRecord::Schema.define(version: 2020_09_17_214234) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
