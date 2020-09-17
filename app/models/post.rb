9fbe70b8c456a658486415587c6460048405087a
Sanad <s_wj_88@hotmail.com>
2020-09-18 01:22:02 +0300

    Add associations to user and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :users
+  belongs_to :user
   has_many :comments
 
   validates :user_id , presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  
   validates :name, presence: true, length: {within: 2..12}
   validates :email, presence: true
   validates :crypted_password, presence: true, length: {within: 4..20}
