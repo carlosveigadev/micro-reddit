46071f2fc78944a093025f03f1293a810f6161f0
wrakc <carlosveiga1618@gmail.com>
2020-09-17 18:31:53 -0300

    add validations to user.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: {whithin: 2..12}
+  validates :email, presence: true
+  validates :crypted_password, presence: true, length: {whithin: 4..20}
 end
