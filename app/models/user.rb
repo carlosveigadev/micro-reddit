2a16aa6417db7871990c8d6713a2bc914cbfcc53
wrakc <carlosveiga1618@gmail.com>
2020-09-17 18:33:53 -0300

    fix type on user.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: {whithin: 2..12}
+  validates :name, presence: true, length: {within: 2..12}
   validates :email, presence: true
-  validates :crypted_password, presence: true, length: {whithin: 4..20}
+  validates :crypted_password, presence: true, length: {within: 4..20}
 end
