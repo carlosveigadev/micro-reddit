a53030a17bb337de2ccad9e19bec56d2a8cfc7bc
Sanad <s_wj_88@hotmail.com>
2020-09-18 18:50:18 +0300

    DB migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_09_17_222834) do
+
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -34,4 +35,5 @@ ActiveRecord::Schema.define(version: 2020_09_17_222834) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
