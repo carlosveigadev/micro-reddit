a0ef5905f505010b6010819148451c686bfc93af
wrakc <carlosveiga1618@gmail.com>
2020-09-18 16:09:35 -0300

    add missing validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,6 @@ class Comment < ApplicationRecord
 
   validates :user_id, presence: true
   validates :post_id, presence: true
+  validates :body, presence: true
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   has_many :comments
 
   validates :user_id, presence: true
+  validates :body, presence: true
+  validates :title, presence: true
 end
